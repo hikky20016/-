8f20d1d2c51051493d90c6d6fd0a90aca25129a2
hikky20016 <0dz192376v0131v@ezweb.ne.jp>
2022-10-20 15:28:52 +0900

    編集機能の追加

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\PostController; 
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスをインポート。
+
+
 
 
 Route::get('/posts', [PostController::class, 'index']); 
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::get('/posts/{post}', [PostController::class ,'show']);
+Route::post('/posts', [PostController::class, 'store']);
+Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
+Route::put('/posts/{post}', [PostController::class, 'update']);
